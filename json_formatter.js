c7ccab9ca2cebc0d6841f122cfb658625f3c6e2b
Heles <heles.hals@gmail.com>
2024-06-06 19:33:18 +0300

    clean

--- json_formatter.js
+++ json_formatter.js
@@ -7,16 +7,15 @@ function formatJSON() {
         // Парсинг введённого JSON
         const jsonObject = JSON.parse(input);
 
-        // Преобразование JSON в форматированную строку
-        // Вывод отформатированного JSON
+
         resultContainer.textContent = JSON.stringify(jsonObject, null, 4);
 
-        // Очистка сообщений об ошибках
+
         errorMessage.textContent = '';
     } catch (error) {
-        // Вывод сообщения об ошибке в случае некорректного JSON
+
         resultContainer.textContent = '';
         errorMessage.textContent = 'Ошибка: Введенные данные не являются корректным JSON.';
-        console.error('JSON parsing error:', error);  // Отладочная информация в консоли
+        console.error('JSON parsing error:', error);
     }
 }
